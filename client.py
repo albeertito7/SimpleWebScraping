9d928d48452a0298a2638935febd52ef8cde366e
albeertito7 <albertperezdatsira@gmail.com>
2017-02-22 14:19:55 +0100

    First commit client

---
+++ client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env-python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf8 :
+
+"""
+Simple client web per saber el Free eBook actual de packtpub.com
+
+@author: albeertito7
+"""
+
+import urllib2
+
+
+class Client(object):
+	pass
+
+
+
+
+if __name__ = __main__:
+	pass
